1957c90ccb0bf94226fa3c82819da22a1356b691
Enrikerf <enrike9066@gmail.com>
2023-06-03 16:44:54 +0000

    test: test Host

---
+++ tests/Unit/Domain/TaskTest/Host/InvalidHostError_test.go
@@ -0,0 +1,26 @@
+package Host
+
+import (
+	"github.com/Enrikerf/pfm/commandManager/app/Domain/Task/Host"
+	"reflect"
+	"testing"
+)
+
+func TestNewInvalidHostError(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Check NewInvalidHostError message",
+			want: "invalid host format",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Host.NewInvalidHostError(); !reflect.DeepEqual(got.Error(), tt.want) {
+				t.Errorf("NewInvalidHostError() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
